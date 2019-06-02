5d0729e8998964ee319c05008e3060e1c27cf00b
1ntegrale9 <st09d23+github@gm.ibaraki-ct.ac.jp>
2019-06-02 15:49:31 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,13 +7,8 @@ token = os.environ['DISCORD_BOT_TOKEN']
 
 
 @bot.event
-async def on_message(message):
-    try:
-        if message.author.bot:
-            return
-        await bot.process_commands(message)
-    except Exception:
-        await message.channel.send(f'```\n{traceback.format_exc()}\n```')
+async def on_command_error(ctx, error):
+    await ctx.send(str(error))
 
 
 @bot.command()
