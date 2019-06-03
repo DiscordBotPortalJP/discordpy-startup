6688ffb434e3a490551889fe78f9f5fa92011644
tundra0 <51240452+tundra0@users.noreply.github.com>
2019-06-04 01:53:28 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,28 +11,26 @@ client = discord.Client()
 #起動時に動作する処理
 @client.event
 async def on_ready():
-	
-#起動したらターミナルに表示されるメッセージ
-		print('Hello World,FRONt LINeナビゲーションbotプログラム「翠ｰスイｰ」、起動しました')
+    print('Hello World,FRONt LINeナビゲーションbotプログラム「翠ｰスイｰ」、起動しました')
 
 #おはよう				
 #メッセージ受信時に動作する処理
 @client.event
 async def on_message(message):
-	#メッセージ送信者がbotだった場合の無視処理
-	if message.author.bot:
-			return 
-	#「おはよう」と発言したら「おはようございます、(送信したユーザーネーム)様！」と返す処理
-	if message.content == 'おはよう':
-			await message.channel.send('おはようございます、' + message.author.name + '様！( ⑉¯ ꇴ ¯⑉ )')			
+    #メッセージ送信者がbotだった場合の無視処理
+    if message.author.bot:
+            return 
+    #「おはよう」と発言したら「おはようございます、(送信したユーザーネーム)様！」と返す処理
+    if message.content == 'おはよう':
+        await message.channel.send('おはようございます、' + message.author.name + '様！( ⑉¯ ꇴ ¯⑉ )')
 #おやすみ		
-	#「おやすみ」と発言したら「おやすみなさい！」と返す処理
-	if message.content == 'おやすみ':
-			await message.channel.send('おやすみなさい、良い夢を見てくださいね！(｡•̀ᴗ-)✧')										
+    #「おやすみ」と発言したら「おやすみなさい！」と返す処理
+    if message.content == 'おやすみ':
+            await message.channel.send('おやすみなさい、良い夢を見てくださいね！(｡•̀ᴗ-)✧')						
 #会話する
-        #「にゃあ」と言ったら猫の真似をする
-        if message.content == 'にゃあ':
-	                await message.content.send('ฅ(*^・ω・^)ฅﾆｬ-')
+    #「にゃあ」と言ったら猫の真似をする			
+    if message.content == 'にゃあ':
+            await message.content.send('ฅ(*^・ω・^)ฅﾆｬ-')	
 												
 #botの起動とdiscordサーバーへの接続
 client.run(TOKEN)
