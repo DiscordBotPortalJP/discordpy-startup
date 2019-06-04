90262b10f0c4ea60f5a19f1f601eaea5fa0d6392
tundra0 <51240452+tundra0@users.noreply.github.com>
2019-06-04 22:30:12 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -23,15 +23,30 @@ async def on_message(message):
     #「おはよう」と発言したら「おはようございます、(送信したユーザーネーム)様！」と返す処理
     if message.content.startswith('おは') or message.content == 'ぐっもーにん':
         await message.channel.send('おはようございます、' + message.author.name + '様！( ⑉¯ ꇴ ¯⑉ )')
-#おやすみ		
+#おやすみ	
+    #メッセージ送信者がbotだった場合の無視処理
+    if message.author.bot:
+            return		
     #「おやすみ」と発言したら「おやすみなさい！」と返す処理
     if message.content == 'おやすみ':
         await message.channel.send('おやすみなさい、良い夢を見てくださいね！(｡•̀ᴗ-)✧')		
 #会話する
+ #にゃあ
+    #メッセージ送信者がbotだった場合の無視処理
+    if message.author.bot:
+            return
     #「にゃあ」と言ったら猫の真似をする			
     if message.content == '翠、猫の真似して欲しい！' or message.content == '翠、癒して！':
         await message.channel.send('ฅ(๑>ㅅ<๑)ฅﾆｬｰ')
-												
+ 
+ #起きてる？
+    #メッセージ送信者がbotだった場合の無視処理
+    if message.auther.bot:
+            return 
+    #「起きてる？」と言ったら返事する
+    if message.content == '翠、起きてる？':
+        await message.channel.send('私はいつも起きてますよ！何かご用ですか？((っ•ω•⊂))')												
+
 #botの起動とdiscordサーバーへの接続
 client.run(TOKEN)
 			
