92d3aa0d023e52568530ea439d0a9ac2d1e5db17
Laguna <test.co.jp@gmail.com>
2019-06-27 22:02:31 +0900

    echo

--- discordbot.py
+++ discordbot.py
@@ -1,19 +1,39 @@
-from discord.ext import commands
+from flask import Flask, request
+import discord
 import os
 import traceback
+import threading
+import asyncio
 
-bot = commands.Bot(command_prefix='/')
+app = Flask(__name__)
+
+client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN']
 
+def startHook():
+    app.run()
 
-@bot.event
-async def on_command_error(ctx, error):
-    await ctx.send(str(error))
+#@app.route("/", methods=['POST'])
+@app.route("/news/events", methods=['POST'])
+def webhook():
+    print(request.get_data())
+    for channel in filter(lambda x : x.type == discord.ChannelType.text, client.get_all_channels()):
+        asyncio.ensure_future(channel.send(request.get_data()), loop=client.loop) 
 
+    return 'OK'
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
 
+@client.event
+async def on_message(message):
+    print(message.content)
 
-bot.run(token)
+thread1 = threading.Thread(target=startHook)
+
+thread1.start()
+client.run(token)
