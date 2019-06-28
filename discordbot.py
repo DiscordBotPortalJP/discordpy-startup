6eca0b2dc7f028d57c38e6eea3c787881bd316e7
Laguna <test.co.jp@gmail.com>
2019-06-29 01:24:02 +0900

    loop

--- discordbot.py
+++ discordbot.py
@@ -10,9 +10,21 @@ def run(token):
     m.run(token)
 
 class Mariage:
-    client = discord.Client()
+    client = None
 
     def run(self, token):
+        self.client = discord.Client()
+        @self.client.event
+        async def on_ready():
+            print('Logged in as')
+            #print(client.user.name)
+            #print(client.user.id)
+            print('------')
+
+        @self.client.event
+        async def on_message(message):
+            print(message.content)
+        
         self.client.run(token)
     
     def broadcast(self, message):
@@ -20,13 +32,3 @@ class Mariage:
         for channel in filter(lambda x : x.type == discord.ChannelType.text, self.client.get_all_channels()):
             asyncio.ensure_future(channel.send(message), loop=self.client.loop)
     
-    @client.event
-    async def on_ready():
-        print('Logged in as')
-        #print(client.user.name)
-        #print(client.user.id)
-        print('------')
-
-    @client.event
-    async def on_message(message):
-        print(message.content)
