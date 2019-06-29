28db4e7a3ef0243252d56233079fc810a6f7cebc
Laguna <test.co.jp@gmail.com>
2019-06-30 01:08:54 +0900

    ph1test

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
 from discordbot import Mariage
+import discord
+import bs4
 import os
 import asyncio
 import threading
@@ -9,13 +11,21 @@ m = Mariage()
 
 @app.route("/news/events", methods=['POST'])
 def webhook():
-    m.broadcast(request.get_data())
+    print(request.json)
+    embed = discord.Embed(
+        title=request.json['feedTitle'],
+        url=request.json['url'])
+    embed.set_author(name=request.json['title'])
+    embed.set_image(url=request.json['imageUrl'])
+    soup = bs4.BeautifulSoup(request.json['content'], "html.parser")
+    embed.add_field(name="おしらせ１", value=soup.div.text[0:1024])
+    m.broadcastEmbed(embed)
 
     return 'OK'
 
-thread = threading.Thread(target=m.run, args=(os.environ['DISCORD_BOT_TOKEN'],))
-thread.start()
-
 if __name__ == "__main__":
+    thread = threading.Thread(target=m.run, args=(os.environ['DISCORD_BOT_TOKEN'],))
+    thread.start()
+
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
--- discordbot.py
+++ discordbot.py
@@ -16,6 +16,7 @@ class Mariage:
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
         self.client = discord.Client()
+
         @self.client.event
         async def on_ready():
             print('Logged in as')
@@ -25,13 +26,40 @@ class Mariage:
 
         @self.client.event
         async def on_message(message):
-            print(message.content)
-        
+            # メッセージ送信者がBotだった場合は無視する
+            if message.author.bot:
+                return
+            # 「/neko」と発言したら「にゃーん」が返る処理
+            if message.content == '/neko':
+                await message.channel.send('にゃーん')
+            # 話しかけた人に返信する
+            if self.client.user in message.mentions: # 話しかけられたかの判定
+                reply = f'{message.author.mention} 呼んだ？' # 返信メッセージの作成
+                await message.channel.send(reply) # 返信メッセージを送信
+            # メンバーのリストを取得して表示
+            if message.content == '/members':
+                print(message.guild.members)
+            # 役職のリストを取得して表示
+            if message.content == '/roles':
+                print(message.guild.roles)
+            # テキストチャンネルのリストを取得して表示
+            if message.content == '/text_channels':
+                print(message.guild.text_channels)
+            # ボイスチャンネルのリストを取得して表示
+            if message.content == '/voice_channels':
+                print(message.guild.voice_channels)
+            # カテゴリチャンネルのリストを取得して表示
+            if message.content == '/category_channels':
+                print(message.guild.categories)
+
         asyncio.ensure_future(self.client.start(token))
         loop.run_forever()
     
     def broadcast(self, message):
-        print(request.get_data())
         for channel in filter(lambda x : x.type == discord.ChannelType.text, self.client.get_all_channels()):
             asyncio.ensure_future(channel.send(message), loop=self.client.loop)
+    
+    def broadcastEmbed(self, embed):
+         for channel in filter(lambda x : x.type == discord.ChannelType.text, self.client.get_all_channels()):
+            asyncio.ensure_future(channel.send(embed=embed), loop=self.client.loop)
     
