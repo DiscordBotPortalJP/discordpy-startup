ccef386cdad10e8335f161b3fa0f283d383b8f84
Laguna <test.co.jp@gmail.com>
2019-06-30 16:02:50 +0900

    beta

--- app.py
+++ app.py
@@ -5,23 +5,48 @@ import bs4
 import os
 import asyncio
 import threading
+import re
 
 app = Flask(__name__)
 m = Mariage()
 
+@app.route("/alive", methods=['GET'])
+def alive():
+    return "OK"
+
 @app.route("/news/events", methods=['POST'])
 def webhook():
     print(request.json)
-    embed = discord.Embed(
-        title=request.json['feedTitle'],
-        url=request.json['url'])
-    embed.set_author(name=request.json['title'])
-    embed.set_image(url=request.json['imageUrl'])
+   
     soup = bs4.BeautifulSoup(request.json['content'], "html.parser")
-    embed.add_field(name="おしらせ１", value=soup.div.text[0:1024])
-    m.broadcastEmbed(embed)
+    
+    for h2 in soup.find_all("h2", id=re.compile("^detail_")):
+        print(h2)
+        
+        description=""
+        for p in filter(lambda e : e.name=="p", findConntents(h2, lambda x : x.name != "h2" and x.name != "h3")):
+            description += p.text
+        embed = discord.Embed(
+            title=h2.text,
+            url=request.json['url'],
+            description=description)
+        embed.set_author(
+            name=request.json['feedTitle'],
+            url=request.json['feedUrl'],
+            icon_url=request.json['author'])
+        for img in filter(lambda e : e.name=="img", findConntents(h2, lambda x : x.name != "h2" and x.name != "h3")):
+            embed.set_image(url=img.attrs["src"])
+            break
+        m.broadcastEmbed(embed)
 
     return 'OK'
+
+def findConntents(elm, pre):
+    for nx in elm.find_all_next():
+        if (pre(nx)):
+            yield nx
+        else:
+            break
 
 thread = threading.Thread(target=m.run, args=(os.environ['DISCORD_BOT_TOKEN'],))
 thread.start()
