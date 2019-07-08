b6041c8b1c68e544ab4e4c537d02908465b26958
aiotter <ogran.std@gmail.com>
2019-07-08 15:42:36 +0900

    Bug fixed on on_error in discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -9,7 +9,7 @@ token = os.environ['DISCORD_BOT_TOKEN']
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
-    error_msg = traceback.format_exception(type(orig_error), orig_error, orig_error.__traceback__)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
 
