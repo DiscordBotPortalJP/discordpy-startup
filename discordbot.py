91e8a05771ff86cbe99543fe82bb83c02d5645b2
cist-b214-k-suzuki <cist.b214.k.suzuki@gmail.com>
2019-08-10 04:47:43 +0900

    ADD otupi bakunetu test

--- discordbot.py
+++ discordbot.py
@@ -12,8 +12,15 @@ async def on_command_error(ctx, error):
 
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def otupi(ctx):
+    await ctx.send('ドラゴンフラッグ')
 
+@bot.command()
+async def bakunetu(ctx):
+    await ctx.send('おちんぽ大好き')
+    
+@bot.command()
+async def test(ctx):
+    await ctx.send('SUCCESS')
 
 bot.run(token)
