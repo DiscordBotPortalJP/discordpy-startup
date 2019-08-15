39f6edf5c01fcef1ceeaceea257836be75fb51f0
sakurada-9999 <54128922+sakurada-9999@users.noreply.github.com>
2019-08-15 13:18:14 +0900

    nekomario

--- discordbot.py
+++ discordbot.py
@@ -15,5 +15,12 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
+@bot.command()
+async def neko(ctx):
+    await ctx.send('にゃーん！')
+
+@bot.command()
+async def nekomario(ctx):
+    await ctx.send('ニャオーッス！')
 
 bot.run(token)
