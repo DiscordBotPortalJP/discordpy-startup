bc11e8df0fe3d32e92fde05dc086f782c401ec3d
amanohashi <48061698+amanohashi@users.noreply.github.com>
2019-11-10 22:26:32 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -412,7 +412,7 @@ url_embed] #ヘルプの各ページ内容
                         embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/635993816297504809/643042178968584192/s-2011112114520719368.jpg')
                         embed.add_field(name="LVup時刻", value=str(dateTime.year)+"/"+str(dateTime.month)+"/"+str(dateTime.day)+"/"+str(dateTime.hour)+"時"+str(dateTime.minute)+"分"+str(dateTime.second)+"秒", inline=False)
 #                        embed.add_field(name="YUI news", value="大幅に改良！\n詳しくはヘルプの第６項から公式鯖へ", inline=True)
-                        embed.set_footer(icon_url=client.author.avatar_url, text="｜")
+                        embed.set_footer(icon_url=client.user.avatar_url, text="｜")
                         await asyncio.gather(*(c.send(embed=embed) for c in client.get_all_channels() if c.name == 'yui_LVUPログ'))
                                                 
                     else:
