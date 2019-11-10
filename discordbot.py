e22088552ba59305730b8057e75d490ceef6341c
amanohashi <48061698+amanohashi@users.noreply.github.com>
2019-11-11 00:49:30 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -434,11 +434,11 @@ url='https://cdn.discordapp.com/attachments/635993816297504809/64309155914291610
                 if not embed.title:
                     pass
     # descriptionは自身が_EmptyEmbedの時Falseを返すのでここの処理
-   
-                elif embed.title.find("が待ち構えている"):
-                    await message.channel.send('::atk　（＾ω＾ ≡ ＾ω＾）おっおっおっ')
                 elif embed.title.find("ゲームにログインしてね") and title.find(f"{client.user.mention}"):
                     await message.channel.send('::login ログインしてなかった') 
+
+                elif embed.title.find("が待ち構えている"):
+                    await message.channel.send('::atk　（＾ω＾ ≡ ＾ω＾）おっおっおっ')
                 else:
                     pass
 
