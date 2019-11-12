b0388d7bd835784c5cc216b590cdbcd0f3dc508c
amanohashi <48061698+amanohashi@users.noreply.github.com>
2019-11-12 18:37:53 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -425,7 +425,7 @@ url_embed] #ヘルプの各ページ内容
                     await atk_ch.send( "::atk 零-zero-" ) 
                 else:
                     pass 
-    if message.author.id == 526620171658330112 or message.author.id == 642271360667877386 and message.channel = atk_ch:
+    if message.author.id == 526620171658330112 or message.author.id == 642271360667877386 and message.channel == atk_ch:
         if len(message.embeds) != 0:             
             for field in embed.fields:
 
