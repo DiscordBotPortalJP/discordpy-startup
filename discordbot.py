6339ac98737b49542493b419a76fd92330b9598f
amanohashi <48061698+amanohashi@users.noreply.github.com>
2019-11-13 18:06:53 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -21,7 +21,7 @@ dateTime = datetime.datetime.now()
 server_number = len(client.guilds)
 client.global_list = [] #グローバルチャット参加チャンネルのリスト
 atk_ch_id = "#掃き溜め"
-atk_ch = client.get_channel(str(643461030692782081))
+atk_ch = client.get_channel(643461030692782081)
 
 help_ch = 642578258743001088
 ModeFlag = 0
