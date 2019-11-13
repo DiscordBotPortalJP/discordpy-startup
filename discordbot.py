a3da7e5abb09c34fe747cc533a9c7be4ac9ccb7a
amanohashi <48061698+amanohashi@users.noreply.github.com>
2019-11-13 17:50:02 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -448,7 +448,10 @@ url_embed] #ヘルプの各ページ内容
                 if f'{client.user.mention}はレベルアップした！' in description :
                     print("LvUP")
                     level_up=description.split(f'{client.user.mention}はレベルアップした！')[1]
-                    embed = discord.Embed(title='YUIがレベルアップ!!\n'+(level_up),color=discord.Colour.green())
+                    embed = discord.Embed(title='━LvUP━\n'+(level_up),color=discord.Colour.green())
+                    embed.set_thumbnail(url=random.choice(('https://media.discordapp.net/attachments/635993816297504809/643091559142916109/videotogif_2019.11.10_23.14.46.gif?width=375&height=375')))
+                    embed.add_field(name="━時刻━", value=str(dateTime.year)+"/"+str(dateTime.month)+"/"+str(dateTime.day)+"\n "+str(dateTime.hour)+"時"+str(dateTime.minute)+"分"+str(dateTime.second)+"秒", inline=False)
+
                     await asyncio.gather(*(c.send(embed=embed) for c in client.get_all_channels() if c.name == 'ログ'))
                 else:
                     print("wow")             
