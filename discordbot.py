1fc012dfa13398d3773ea688fb45aec3f1e49455
Amanohashi <48061698+amanohashi@users.noreply.github.com>
2019-11-14 23:16:54 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -408,7 +408,7 @@ url_embed] #ヘルプの各ページ内容
     if message.author.id == 526620171658330112 or message.author.id == 642271360667877386 :
         if len(message.embeds) != 0:             
             for embed in message.embeds:
-                discription = embed.discription
+                description = embed.description
                 if '?' in embed.description and message.channel == q_ch: 
                     await message.add_reaction("1️⃣")
                     def  hellocheck(m):
