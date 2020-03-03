67b1b9d803efe5134c199b7acfee51b3f4ef8343
tsukine <61737877+tukine@users.noreply.github.com>
2020-03-03 21:21:07 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,6 +16,10 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+    
+@bot.command()
+async def ping(ctx):
+    await ctx.send('にゃーん')
 
 
-bot.run(token)
+bot.run(nMtzpktofsH1rGAhVZdQkfq3oWbQpCT0)
