ccda0985e1f672b8777016679eb21a22fd464672
Midorichan <55083684+Midorichaan@users.noreply.github.com>
2020-05-28 18:57:27 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -9,6 +9,7 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 bot.teams = [546682137240403984]
 
+bot.globalc = True
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -23,38 +24,38 @@ async def ping(ctx):
 
 @bot.event
 async def on_message(message):
-    try:
-        if message.author.bot:
+    if bot.globalc == True:
+        try:
+            if message.author.bot:
             # もし、送信者がbotなら無視する
-            return
-        GLOBAL_CH_NAME = "mikan-global" # グローバルチャットのチャンネル名
+                return
+            GLOBAL_CH_NAME = "mikan-global" # グローバルチャットのチャンネル名
 
-        if isinstance(message.channel,discord.TextChannel) and message.channel.name == GLOBAL_CH_NAME:
+            if isinstance(message.channel,discord.TextChannel) and message.channel.name == GLOBAL_CH_NAME:
             # hoge-globalの名前をもつチャンネルに投稿されたので、メッセージを転送する
 
-            await message.delete() # 元のメッセージは削除しておく
+                await message.delete() # 元のメッセージは削除しておく
 
-            channels = bot.get_all_channels()
-            global_channels = [ch for ch in channels if ch.name == GLOBAL_CH_NAME]
+                channels = bot.get_all_channels()
+                global_channels = [ch for ch in channels if ch.name == GLOBAL_CH_NAME]
             # channelsはbotの取得できるチャンネルのイテレーター
             # global_channelsは hoge-global の名前を持つチャンネルのリスト
 
-            embed = discord.Embed(title=GLOBAL_CH_NAME,
-                description=message.content, color=0x00bfff)
+                embed = discord.Embed(title=GLOBAL_CH_NAME, description=message.content, color=0x00bfff)
 
-            embed.set_author(name=message.author.display_name, 
-                icon_url=message.author.avatar_url_as(format="png"))
-            embed.set_footer(text=f"{message.guild.name} / {message.channel.name}",
-                icon_url=message.guild.icon_url_as(format="png"))
+                embed.set_author(name=message.author.display_name, icon_url=message.author.avatar_url_as(format="png"))
+                embed.set_footer(text=f"{message.guild.name} / {message.channel.name}",icon_url=message.guild.icon_url_as(format="png"))
             # Embedインスタンスを生成、投稿者、投稿場所などの設定
 
-            for channel in global_channels:
+                for channel in global_channels:
                 # メッセージを埋め込み形式で転送
-                await channel.send(embed=embed)
-        else:
-            await bot.process_commands(message)
-    except Exception as error:
-        await on_command_error(message.channel,error)
+                    await channel.send(embed=embed)
+            else:
+                await bot.process_commands(message)
+        except Exception as error:
+            await on_command_error(message.channel,error)
+    else:
+        return
 
 @bot.command(pass_context=True)
 async def ping2(ctx):  # 処理時間を返す
