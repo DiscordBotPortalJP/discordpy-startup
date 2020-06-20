e15b97b5c39204d0a25ddb8f01565fbec787b243
keitomo <47211957+keitomo@users.noreply.github.com>
2020-06-21 00:46:35 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,20 +1,12 @@
 from discord.ext import commands
+import discord
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
-on_message(message):
+@client.event
+async def on_message(message):
     
     if message.content.startswith('ねこ'):
         await message.channel.send('にゃーん')
