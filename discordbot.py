f56c39e45ad7ffed51b8c12b9f9e070ca03fbfec
keitomo <47211957+keitomo@users.noreply.github.com>
2020-06-21 00:52:38 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,24 +1,27 @@
-from discord.ext import commands
+import discord
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
 
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-@bot.command()
-async def neko(ctx):
-    await ctx.send('にゃーん')
-
-bot.run(token)
+# Botの起動とDiscordサーバーへの接続
+client.run(token)
