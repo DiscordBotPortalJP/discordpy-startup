d3d70fc4574fcb86fb3a3c72656f90dde55e7e7e
keitomo <47211957+keitomo@users.noreply.github.com>
2020-06-21 00:55:37 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,12 +7,6 @@ token = os.environ['DISCORD_BOT_TOKEN']
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
 
-# 起動時に動作する処理
-@client.event
-async def on_ready():
-    # 起動したらターミナルにログイン通知が表示される
-    print('ログインしました')
-
 # メッセージ受信時に動作する処理
 @client.event
 async def on_message(message):
@@ -22,6 +16,11 @@ async def on_message(message):
     # 「/neko」と発言したら「にゃーん」が返る処理
     if message.content == '/neko':
         await message.channel.send('にゃーん')
+    
+    if message.content.startswith('calc'):
+        phrase=message.content.split()
+        formula=phrase[1]
+        await message.channel.send(eval(formula))
 
 # Botの起動とDiscordサーバーへの接続
 client.run(token)
