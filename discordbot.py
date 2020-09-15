832a0cae36c72adb5b86ed873a9625248664ec88
tatudon-321 <69696560+tatudon-321@users.noreply.github.com>
2020-09-15 22:27:33 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
+bot = commands.Bot(command_prefix='t!')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
