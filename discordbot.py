f02334b653417354432027456470fa54481e7889
Keriikuua <foxhnd1@gmail.com>
2020-10-17 19:25:49 +0900

    読み上げ機能

--- discordbot.py
+++ discordbot.py
@@ -16,6 +16,42 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+    
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+
+@client.command()
+async def join(ctx):
+    print('#voicechannelを取得')
+    vc = ctx.author.voice.channel
+    print('#voicechannelに接続')
+    await vc.connect()
+
+@client.command()
+async def bye(ctx):
+    print('#切断')
+    await ctx.voice_client.disconnect()
+
+@client.event
+async def on_message(message):
+    msgclient = message.guild.voice_client
+    if message.content.startswith('.'):
+        pass
+
+    else:
+        if message.guild.voice_client:
+            print(message.content)
+            creat_WAV(message.content)
+            source = discord.FFmpegPCMAudio("output.wav")
+            message.guild.voice_client.play(source)
+        else:
+            pass
+    await client.process_commands(message)
 
 
 bot.run(token)
