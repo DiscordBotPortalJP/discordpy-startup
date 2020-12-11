6fffd007b30f55e9ff5f3c034c4b56c121d1d5f9
DiscordBotCreateUser <75853998+DiscordBotCreateUser@users.noreply.github.com>
2020-12-12 03:28:03 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,16 +6,49 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+#@bot.event
+#async def on_command_error(ctx, error):
+#    orig_error = getattr(error, "original", error)
+#    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+#    await ctx.send(error_msg)
 
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+#@bot.command()
+#async def ping(ctx):
+#    await ctx.send('pong')
+
+# コマンドに対応するリストデータを取得する関数を定義
+def get_data(message):
+    command = message.content
+    data_table = {
+        '/members': message.guild.members, # メンバーのリスト
+        '/roles': message.guild.roles, # 役職のリスト
+        '/text_channels': message.guild.text_channels, # テキストチャンネルのリスト
+        '/voice_channels': message.guild.voice_channels, # ボイスチャンネルのリスト
+        '/category_channels': message.guild.categories, # カテゴリチャンネルのリスト
+    }
+    return data_table.get(command, '無効なコマンドです')
+
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
+
+    # 「/mute」と発言したら「ミュート」にする
+    if message.content == '/mute':
+        await message.channel.send('ミュート')
+
+    # 「/meeting」と発言したら「ミュート」を解除する
+    if message.content == '/meeting':
+        await message.channel.send('会議')
+
+    # コマンドに対応するデータを取得して表示
+    print(get_data(message))
 
 
 bot.run(token)
