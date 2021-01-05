f1d3db542c40b36ad79114bec39ea0c4d1b83ad8
jelly-prog <76945483+jelly-prog@users.noreply.github.com>
2021-01-05 19:05:04 +0900

    チーム分けコマンド追加

--- discordbot.py
+++ discordbot.py
@@ -1,18 +1,55 @@
-from discord.ext import commands
 import os
 import traceback
 
+import discord
+from discord.ext import commands
+
+from modules.grouping import 
+
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
+"""ここまで起動処理"""
 
-
+"""イベント検知"""
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
+    
+@bot.event
+async def on_ready():
+    print('-----Logged in info-----')
+    print(bot.user.name)
+    print(bot.user.id)
+    print(discord.__version__)
+    print('------------------------')
 
 
+"""チーム分けコマンド"""
+# メンバー数が均等になるチーム分け
+@bot.command()
+async def team(ctx, specified_num=2):
+    make_team = MakeTeam()
+    remainder_flag = 'true'
+    msg = make_team.make_party_num(ctx,specified_num,remainder_flag)
+    await ctx.channel.send(msg)
+
+# メンバー数が均等にはならないチーム分け
+@bot.command()
+async def team_norem(ctx, specified_num=2):
+    make_team = MakeTeam()
+    msg = make_team.make_party_num(ctx,specified_num)
+    await ctx.channel.send(msg)
+
+# メンバー数を指定してチーム分け
+@bot.command()
+async def group(ctx, specified_num=1):
+    make_team = MakeTeam()
+    msg = make_team.make_specified_len(ctx,specified_num)
+    await ctx.channel.send(msg)
+    
+"""テストコマンド"""
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
