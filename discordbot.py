6dc7579cdc375a15d5faf21d639daa1d2f29b61a
sumaneko <77718010+sumaneko@users.noreply.github.com>
2021-01-20 19:01:48 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -15,7 +15,7 @@ async def on_command_error(ctx, error):
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('pong')
+    await ctx.send('chinpong')
     
 @bot.command()
 client.on('message', message =>{
