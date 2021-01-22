bb439fe041c4aca423df5e0f61715a6586c7c87c
sumaneko <77718010+sumaneko@users.noreply.github.com>
2021-01-22 22:45:19 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -18,4 +18,38 @@ async def ping(ctx):
     await ctx.send('chinpong')
     
     
+"""
+
+client.on('message', message =>{
+  if (message.author.id == client.user.id || message.author.bot){
+    return;
+  }
+  if (message.content.match(/^！おみくじ/) ||
+      (message.isMemberMentioned(client.user) && message.content.match(/おみくじ/))){
+    let arr = ["大吉", "吉", "凶", "ぽてと", "にゃ～ん", "しゅうまい君"];
+    let weight = [5, 30, 10, 15, 20, 20];
+    lotteryByWeight(message.channel.id, arr, weight);
+  }else if (message.isMemberMentioned(client.user)){
+    sendReply(message, "呼びましたか？");
+  }
+});
+
+function lotteryByWeight(channelId, arr, weight){
+  let totalWeight = 0;
+  for (var i = 0; i < weight.length; i++){
+    totalWeight += weight[i];
+  }
+  let random = Math.floor(Math.random() * totalWeight);
+  for (var i = 0; i < weight.length; i++){
+    if (random < weight[i]){
+      sendMsg(channelId, arr[i]);
+      return;
+    }else{
+      random -= weight[i];
+    }
+  }
+  console.log("lottery error");
+}
+"""
+    
 bot.run(token)
