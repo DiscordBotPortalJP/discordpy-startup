f6382a96a46b4495d98345ba4e018ed5008f7b6c
sumaneko <77718010+sumaneko@users.noreply.github.com>
2021-01-23 16:26:57 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,11 +1,11 @@
 from discord.ext import commands
 import os
 import traceback
+import harapan(bot)
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-import harapan(bot)
 
 @bot.event
 async def on_command_error(ctx, error):
