1976011cf76c4777e1deb04ca8df94073f11d5b9
sumaneko <77718010+sumaneko@users.noreply.github.com>
2021-01-23 21:42:38 +0900

    Update bot_reaction.py

--- bot_reaction.py
+++ bot_reaction.py
@@ -1,5 +1,9 @@
 def get_bot_reaction(msg) :
     reaction = ""
+    if "おっぱい" in msg:
+        reaction = "おっぱい！"
+    if "まんこ" in msg:
+        reaction = "おまんこ！"
     if "アナル" in msg:
         reaction = "アナル！"
     return reaction
