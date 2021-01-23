cd9ab22c978686e1abc5ea9015b5b321c9d608f4
sumaneko <77718010+sumaneko@users.noreply.github.com>
2021-01-24 01:42:25 +0900

    Update bot_reaction.py

--- bot_reaction.py
+++ bot_reaction.py
@@ -8,11 +8,12 @@ def get_bot_reaction(msg) :
         return reaction
     
     #下ネタは30%の確率で反応
-    if random.randint(0, 100) < 30:
+    if random.randint(0, 100) < 100:
         reaction = shimoneta(msg)
     
     return reaction
 
+#挨拶
 def greetings(m):
     t = ""
     if m.content == "おはー":
@@ -29,12 +30,47 @@ def greetings(m):
         t = "寝るんですか？私も寝たい！おやすみ！"
     return t
 
+#下ネタ
 def shimoneta(m):
     t = ""
+    #コピペ用[100, ""],
     if "おっぱい" in m.content:
-        t = "おっぱい！"
+        quotes = [
+            [100, "おっぱい！"],
+            [100, "おっぱいおっぱい"],
+            [100, m.author.name + "さんっておっぱい大好きですよね"]
+        ]
+        t = get_quotes(quotes)
+        
     if "まんこ" in m.content:
-        t = "おまんこ！"
+        quotes = [
+            [100, "おまんこ！"],
+            [100, "やだなぁエッチなんだから・・・"],
+            [100, "おまんこなんて言ってたら引かれちゃいますよ？"]
+        ]
+        t = get_quotes(quotes)
+        
     if "アナル" in m.content:
-        t = "アナル！"
+        quotes = [
+            [100, "アナル！"],
+            [100, m.author.name + "さんもアナル好きなんですか？"],
+            [100, "お尻は繊細ですから、優しく使わないと切れちゃいますよ？"],
+        ]
+        t = get_quotes(quotes)
     return t
+
+#配列から確率でセリフを取り出す
+def get_quotes(q_list):
+    total_weight = 0
+    for i in q_list:
+        total_weight += i[0]
+    
+    #順番に確率に当てはまるかやっていく
+    target = q_list[0]
+    for i in q_list:
+        if  random.randint(0, total_weight) <= i[0]:
+            target = i
+            break
+        total_weight -= i[0]
+    return target[1]
+        
