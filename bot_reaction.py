68a5409ea7bd4f8ab0e73eda6b89b51ea4b492b9
sumaneko <77718010+sumaneko@users.noreply.github.com>
2021-01-24 14:15:27 +0900

    Update bot_reaction.py

--- bot_reaction.py
+++ bot_reaction.py
@@ -117,7 +117,15 @@ def shimoneta(m):
         ]
         t = get_quotes(quotes)
         
-    if "ちんこ" in m.content or "チンチン" in m.content or "ちんちん" in m.content or "チンポ" in m.content:
+    if "ちんこ" in m.content or "チンチン" in m.content:
+        quotes = [
+            [100, "え、おちんちん・・・？],
+            [100, "ちーんちん"],
+            [100, "ち・・・チンポ"]
+        ]
+        t = get_quotes(quotes)
+            
+    if "ちんちん" in m.content or "チンポ" in m.content:
         quotes = [
             [100, "え、おちんちん・・・？],
             [100, "ちーんちん"],
