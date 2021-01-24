440fd7209e48f00f8c4c2cb1880eb24767edba0f
sumaneko <77718010+sumaneko@users.noreply.github.com>
2021-01-24 15:53:06 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,24 +2,28 @@ from discord.ext import commands
 import os
 import traceback
 
+#汎用モジュール
 import random
 
+#次作モジュール
 import harapan_quotes
 import bot_reaction
 
+#BOTをコンストラクト
 bot = commands.Bot(command_prefix='/')
+#動かすにはトークンが必要
 token = os.environ['DISCORD_BOT_TOKEN']
 
-
+#エラーだけど吐かせないでコメントで終わり
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     msg = "そんなの知らないです・・・"
     await ctx.send(msg)
-#await ctx.send(error_msg)
+    #await ctx.send(error_msg)
 
-
+#自分でコマンドを作れる
 @bot.command()
 async def ping(ctx):
     await ctx.send("chinpong")
@@ -35,15 +39,15 @@ async def harapan(ctx):
 #発言に反応する
 @bot.event
 async def on_message(message):
+	#発言者がBOTなら即終了
 	if message.author.bot:
         	return
-	#どうも呼び出し先が長過ぎるとエラーになって止まる模様
-	msg = bot_reaction.get_greetings(message)
-	if msg == "":
-		msg = bot_reaction.get_shimoneta(message)
+
+	msg = bot_reaction.get_bot_reaction(message)
+	
 	if msg != "":
 		await message.channel.send(msg)
-	await bot.process_commands(message)
-	
+	await bot.process_commands(message)	
 
+#起動
 bot.run(token)
