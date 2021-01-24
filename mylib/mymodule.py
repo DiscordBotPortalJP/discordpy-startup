91a85a583b3425cfed868294f0a7c7343c3f33f3
sumaneko <77718010+sumaneko@users.noreply.github.com>
2021-01-25 02:09:03 +0900

    Create mymodule.py

---
+++ mylib/mymodule.py
@@ -0,0 +1,17 @@
+import random
+
+#配列から確率でセリフを取り出す
+#構造は[[重み, "セリフ"], ]
+def get_quotes(q_list):
+    total_weight = 0
+    for i in q_list:
+        total_weight += i[0]
+    
+    #順番に確率に当てはまるかやっていく
+    target = q_list[0]
+    for i in q_list:
+        if  random.randint(0, total_weight) <= i[0]:
+            target = i
+            break
+        total_weight -= i[0]
+    return target[1]
