78aa630d5ad48d4579b2a6a0f78423dd8b1f8b8c
sumaneko <77718010+sumaneko@users.noreply.github.com>
2021-01-24 14:32:27 +0900

    Update bot_reaction.py

--- bot_reaction.py
+++ bot_reaction.py
@@ -140,6 +140,7 @@ if "ちんこ" in m.content or "チンチン" in m.content:
 
 def shimoneta2(m):
     t = ""
+    """
     if "ちんこ" in m.content or "チンチン" in m.content or"ちんちん" in m.content or "チンポ" in m.content:
         quotes = [
             [100, "え、おちんちん・・・？],
@@ -147,6 +148,7 @@ def shimoneta2(m):
             [100, "ち・・・チンポ"]
         ]
         t = get_quotes(quotes)
+        """
     return t
 
 #配列から確率でセリフを取り出す
