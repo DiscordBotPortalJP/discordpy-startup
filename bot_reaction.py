cf9bb46f8d1cec0ee474880036bf7e9ed10aa62f
sumaneko <77718010+sumaneko@users.noreply.github.com>
2021-01-24 22:35:27 +0900

    Update bot_reaction.py

--- bot_reaction.py
+++ bot_reaction.py
@@ -4,12 +4,27 @@ def get_bot_reaction(msg) :
     reaction = ""
     #挨拶
     reaction = greetings(msg)
+    #合致したら強制終了
     if reaction != "":
         return reaction
+    
+    #慰める
+    reaction = cheer(msg)
+    #合致したら終了
+    if reaction != "":
+        return reaction
+    
+    #リョナ
+    reaction = ryona(msg)
     
     #下ネタは30%の確率で反応あるいは１００％でも別にいいか
     if random.randint(0, 100) < 100:
         reaction = shimoneta(msg)
+        if reaction != "":
+            return reaction
+    
+    #趣味関連
+    reaction = hobby(msg)
     
     return reaction
 
@@ -61,6 +76,38 @@ def greetings(m):
 def shimoneta(m):
     t = ""
     #コピペ用[100, ""],
+    if "ロリ" in m.content:
+        quotes = [
+            [100, "いいですよねロリ"],
+            [100, "ロリ好き！"],
+            [100, "このロリコン！"]
+        ]
+        t = get_quotes(quotes)
+        
+    if "ちんこ" in m.content or "チンチン" in m.content or "ちんちん" in m.content or "チンポ" in m.content:
+        quotes = [
+            [100, "え、おちんちん・・・？"],
+            [100, "ちーんちん"],
+            [100, "ち・・・チンポ"]
+        ]
+        t = get_quotes(quotes)
+        
+    if "おしっこ" in m.content or "シッコ" in m.content or "小便" in m.content:
+        quotes = [
+            [100, "おしっこ！"],
+            [100, "飲みますか？"],
+            [100, "直飲みは無菌と言われてましたが実はそんなこと無いらしいですよ"]
+        ]
+        t = get_quotes(quotes)
+        
+    if "潮吹き" in m.content or "お潮" in m.content:
+        quotes = [
+            [100, "潮吹きはえっち！"],
+            [100, "ぷしゃああって出てるの好き！"],
+            [100, "潮吹きしやすくする訓練法があるんだとか"]
+        ]
+        t = get_quotes(quotes)
+        
     if "おっぱい" in m.content or "乳首" in m.content:
         quotes = [
             [100, "おっぱい！"],
@@ -75,6 +122,14 @@ def shimoneta(m):
             [100, "おまんこ！"],
             [100, "やだなぁエッチなんだから・・・"],
             [100, "おまんこなんて言ってたら引かれちゃいますよ？"]
+        ]
+        t = get_quotes(quotes)
+        
+    if "セックス" in m.content or "おせっせ" in m.content:
+        quotes = [
+            [100, "せっくす！"],
+            [100, "え、セックス？・・・ドキドキ"],
+            [100, "セッ・・・や、優しくしてね？"]
         ]
         t = get_quotes(quotes)
         
@@ -109,20 +164,53 @@ def shimoneta(m):
         ]        
         t = get_quotes(quotes)
         
-    if "ロリ" in m.content:
+    return t
+
+#慰める
+def cheer(m):
+    if m.content == "疲れた" or m.content == "もう疲れた" or "もうやだ" in m.content or "もうだめ" in m.content:
         quotes = [
-            [100, "いいですよねロリ"],
-            [100, "ロリ好き！"],
-            [100, "このロリコン！"]
-        ]
+            [100, "休むのは大事ですよ"],
+            [100, "頑張ってください！"],
+            [100, "気分転換でもしましょう？"]
+        ]        
+        t = get_quotes(quotes)
+    if m.content == "死にたい" or m.content == "もう死にたい":
+        quotes = [
+            [100, "そんなこと言わないでください・・・"],
+            [100, m.author.name + "さんに死んでほしくない人はきっとたくさんいますよ"],
+            [100, "死んじゃだめですよ・・・"]
+        ]        
         t = get_quotes(quotes)
         
-    if "ちんこ" in m.content or "チンチン" in m.content or "ちんちん" in m.content or "チンポ" in m.content:
+    return t
+
+def ryona(m):
+    if "四肢切断" in m.content:
         quotes = [
-            [100, "え、おちんちん・・・？"],
-            [100, "ちーんちん"],
-            [100, "ち・・・チンポ"]
-        ]
+            [100, "四肢がなければ軽くなりますね！"],
+            [100, "指詰めただけでも傷がその後の人生ずっとめっちゃ痛くてつらいらしいのに、四肢なんてどうなっちゃうんでしょうね？"],
+            [100, "オナホにでもしますか？"]
+        ]        
+        t = get_quotes(quotes)
+        
+    if "ギロチン" in m.content or "クビチョンパ" in m.content or "首チョンパ" in m.content or "斬首" in m.content:
+        quotes = [
+            [100, "首チョンパいいですよね・・・なんというか「死んだ」って感じで"],
+            [100, "まだ使える"],
+            [100, "バッサリ落ちるのもいいけど、切れない刃物で何度も切りつけられるのも好き"]
+        ]        
+        t = get_quotes(quotes)
+        
+    return t 
+
+#趣味
+def hobby(m):
+    if "ドーモ" in m.content or "ニンジャ" in m.content or "アイサツ" in m.content:
+        quotes = [
+            [100, "ドーモ、" + m.author.name + "＝サン。リョナゲボットです。"],
+            [100, "ニンジャナンデ！？"]
+        ]        
         t = get_quotes(quotes)
         
     return t
