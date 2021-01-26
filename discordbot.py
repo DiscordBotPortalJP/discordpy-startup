b2be5b80f251c15c697f6ec6097aad69039c353c
sumaneko <77718010+sumaneko@users.noreply.github.com>
2021-01-27 01:48:26 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -10,20 +10,20 @@ bot = commands.Bot(command_prefix='/')
 #動かすにはトークンが必要
 token = os.environ['DISCORD_BOT_TOKEN']
 
-locked = false
+locked = False
 
 #発言に反応する
 @bot.event
 async def on_message(message):
-        global locked
-        if locked:
-            return
-
-        locked = True
-        await ctx.send("テスト中")
-        await asyncio.sleep(100)
-        locked = False
+    global locked
+    if locked:
         return
+
+    locked = True
+    await ctx.send("テスト中")
+    await asyncio.sleep(100)
+    locked = False
+    return
 
 #起動
 bot.run(token)
