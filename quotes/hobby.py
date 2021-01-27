917adcc67bd89d68b99fe75e7a978f0612138ca3
sumaneko <77718010+sumaneko@users.noreply.github.com>
2021-01-28 03:08:44 +0900

    Update hobby.py

--- quotes/hobby.py
+++ quotes/hobby.py
@@ -15,4 +15,26 @@ def get_hobby(m):
             [100, "ヴォフンヴォフン"]
         ]
         t = get_quotes(quotes)
+        
+    if "尻尾" in m.content or "しっぽ" in m.content:
+        quotes = [
+            [100, "尻尾があると走る時結構便利なんですよ"],
+            [100, "尻尾は背骨の延長なので、強く引っ張らないでくださいね？死んじゃうから"],
+            [100, "ほら尻尾ありますよ？もふもふですよ？"]
+        ]
+        t = get_quotes(quotes)
+        
+    if "ネコミミ" in m.content or "猫耳" in m.content or "ネコ耳" in m.content or "獣耳" in m.content or "ケモミミ" in m.content or "ケモノミミ" in m.content:
+        quotes = [
+            [100, "私にも猫耳生えてますよ！ほらほら、触りますか？"],
+            [100, "ケモミミとか好きなんですか？"],
+            [100, "猫耳生えてる分脳みそ少ないんじゃないの？とか言う人がいるんですよね！失礼しちゃう！"]
+        ]
+        
+    if "ネコ" in m.content or "猫" in m.content or "にゃー" in m.content or "にゃん" in m.content:
+        quotes = [
+            [100, "にゃー！"],
+            [100, "にゃんにゃん！"]
+        ]
+        t = get_quotes(quotes)
     return t
