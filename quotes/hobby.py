813797c4f67442ea6c9175ecc4a8338e4c3a8fc6
sumaneko <77718010+sumaneko@users.noreply.github.com>
2021-01-27 23:38:23 +0900

    Create hobby.py

---
+++ quotes/hobby.py
@@ -0,0 +1,18 @@
+from mylib.mymodule import get_quotes
+#趣味
+def hobby(m):
+    t = ""
+    if "ドーモ" in m.content or "ニンジャ" in m.content or "アイサツ" in m.content:
+        quotes = [
+            [100, "ドーモ、" + m.author.name + "＝サン。リョナゲボットです。"],
+            [100, "ニンジャナンデ！？"]
+        ]        
+        t = get_quotes(quotes)
+    if m.content == "ぼふんぼふん":
+        quotes = [
+            [100, "ぼふんぼふん"],
+            [30, "ほらもっと啼いてごらんなさい？厚かましいげっ歯類めが！！"],
+            [100, "ヴォフンヴォフン"]
+        ]
+        t = get_quotes(quotes)
+    return t
