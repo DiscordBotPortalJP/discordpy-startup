9d6b69fd3f85b5dcbd27ebb5a3b4a6c52abdd933
sumaneko <77718010+sumaneko@users.noreply.github.com>
2021-01-29 00:04:01 +0900

    Update ryonage_bot.py

--- mymodule/ryonage_bot.py
+++ mymodule/ryonage_bot.py
@@ -1,5 +1,17 @@
 class RyonageBot:
+	max_hp = 150
 	def __init__(self):
 		self.hp = 150
+		
 	def get_hp(self):
+		return self.hp
+	
+	def damage(self, n):
+		self.hp -= n
+		return self.hp
+	
+	def heal(self, n):
+		self.hp += n
+		if max_hp < self.hp:
+			self.hp = 150
 		return self.hp
