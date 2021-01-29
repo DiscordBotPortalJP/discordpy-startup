c3a5dc0d16c2409eaa28694b74deb7b4c45671b5
sumaneko <77718010+sumaneko@users.noreply.github.com>
2021-01-29 23:26:12 +0900

    Update omorashi.py

--- quotes/omorashi.py
+++ quotes/omorashi.py
@@ -1,44 +1,12 @@
 from mylib.mymodule import get_quotes
 #挨拶
 def get_greetings(m):
-    t = ""
-    if m.content == "おはー" or "おはよ" in m.content:
-        quotes = [
-            [100, "おはー"],
-            [100, m.author.name + "さんおはー"],
-            [100, "おはよ～"]
-        ]
-        t = get_quotes(quotes)
-        
-    if m.content == "こんー":
-        quotes = [
-            [100, "こんー"],
-            [100, "こんこん"],
-            [100, "こん～"]
-        ]
-        t = get_quotes(quotes)
-        
-    if "こんにち" in m.content:
-        quotes = [
-            [100, "こんにちは！"],
-            [100, "こんにちー"]
-        ]
-        t = get_quotes(quotes)
-        
-    if "こんば" in m.content:
-        quotes = [
-            [100, "こんばんは！"],
-            [100, "こんばんはー"],
-            [100, "こん～"]
-        ]
-        t = get_quotes(quotes)
-        
-    if "おやす" in m.content:
-        quotes = [
-            [100, "おやすみなさい～"],
-            [100, "寝るんですか？私も寝たい！おやすみ！"],
-            [100, "睡眠は大事ですよ！"],
-            [100, "寝るおやす！"]
-        ]
-        t = get_quotes(quotes)
-    return t
+	t = ""
+if "おやす" in m.content:
+quotes = [
+[100, "おやすみなさい～"],
+[100, "寝るんですか？私も寝たい！おやすみ！"],
+[100, "睡眠は大事ですよ！"],
+[100, "寝るおやす！"]]
+t = get_quotes(quotes)
+return t
