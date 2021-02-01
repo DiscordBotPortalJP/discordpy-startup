455ad5c19034a4042f106ffc63f3884a7ff5f43f
sumaneko <77718010+sumaneko@users.noreply.github.com>
2021-02-02 02:27:39 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -46,8 +46,7 @@ async def on_command_error(ctx, error):
 #@commands.cooldown(1, 30, commands.BucketType.user（とかchannelとかserver）)でクールダウン仕込める
 @bot.command()
 async def ping(ctx):
-	await ctx.send("<:standup:805846231851073647> ")
-	#await ctx.send("_chinpong_")
+	await ctx.send("_chinpong_")
 	
 @bot.command()
 async def omanko(ctx):
