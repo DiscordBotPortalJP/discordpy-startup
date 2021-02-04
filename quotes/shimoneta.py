585fdb7cc6acf4d8d9856f76f2e7b7b60b24bd0b
sumaneko <77718010+sumaneko@users.noreply.github.com>
2021-02-04 19:41:05 +0900

    Update shimoneta.py

--- quotes/shimoneta.py
+++ quotes/shimoneta.py
@@ -161,6 +161,7 @@ def get_shimoneta(m):
     if "リョナ" in m.content:
         #あまりに反応するから確率を下げる
         if random.randint(1,50) < 50:
+            print("ryona")
             quotes = [
                 [100, "リョナしゅき！"],
                 [100, "リョナが好きだなんてすっっごい変態さんですね！"],
