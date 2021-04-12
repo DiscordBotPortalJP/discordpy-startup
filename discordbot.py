8214209244c50817bf0c290cc3b7f4902764f9ee
developgao <82287776+developgao@users.noreply.github.com>
2021-04-12 18:23:09 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,12 +2,12 @@ from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
+bot = commands.Bot(command_prefix='l')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-if not discord.opus.is_loaded(): 
-    #もし未ロードだったら
-    discord.opus.load_opus("heroku-buildpack-libopus")
+#if not discord.opus.is_loaded(): 
+#    #もし未ロードだったら
+#    discord.opus.load_opus("heroku-buildpack-libopus")
     
 #@bot.command() 
 #async def join(ctx):
@@ -38,8 +38,8 @@ async def ping(ctx):
 @bot.command()
 async def pi(ctx):
     await ctx.send('p')
-    voice_state = ctx.author.voice
-    channel = voice_state.channel #送信者のチャンネル
-    await channel.connect() #VoiceChannel.connect()を使用
+#    voice_state = ctx.author.voice
+#    channel = voice_state.channel #送信者のチャンネル
+#    await channel.connect() #VoiceChannel.connect()を使用
 
 bot.run(token)
