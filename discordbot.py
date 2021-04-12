a9c2462697975b511aa1a5a31d83164018506c7d
developgao <82287776+developgao@users.noreply.github.com>
2021-04-12 19:18:06 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,10 +1,9 @@
 import discord
-import traceback
 from discord.ext import commands
 
 import os
 
-bot = commands.Bot(command_prefix="$")
+bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 if not discord.opus.is_loaded():
