6f8356e9d1532f1a4fe5094f2f6e114724aa18c3
developgao <82287776+developgao@users.noreply.github.com>
2021-04-12 19:06:56 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,24 +5,6 @@ import traceback
 bot = commands.Bot(command_prefix='l')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-#if not discord.opus.is_loaded(): 
-#    #もし未ロードだったら
-#    discord.opus.load_opus("heroku-buildpack-libopus")
-    
-#@bot.command() 
-#async def join(ctx):
-#    """Botをボイスチャンネルに入室させます。"""
-#    voice_state = ctx.author.voice
-#
-#    if (not voice_state) or (not voice_state.channel):
-        #もし送信者がどこのチャンネルにも入っていないなら
-#        await ctx.send("先にボイスチャンネルに入っている必要があります。")
-#        return
-
-#    channel = voice_state.channel #送信者のチャンネル
-
-#    await channel.connect() #VoiceChannel.connect()を使用
-
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
@@ -38,8 +20,5 @@ async def ping(ctx):
 @bot.command()
 async def pi(ctx):
     await ctx.send('p')
-#    voice_state = ctx.author.voice
-#    channel = voice_state.channel #送信者のチャンネル
-#    await channel.connect() #VoiceChannel.connect()を使用
 
 bot.run(token)
