ccb19e4ba94d45491dd56e56fce7c4834924d581
abcdeSky <smiski.career@gmail.com>
2021-05-16 23:29:04 +0900

    updated

--- discordbot.py
+++ discordbot.py
@@ -1,4 +1,4 @@
-from discord.ext import commands
+from discord.ext import commands, tasks
 import os
 import traceback
 
@@ -13,9 +13,17 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
+@tasks.loop(seconds=5.0, count=5)
+async def slow_count():
+    print(slow_count.current_loop)
+
+@slow_count.after_loop
+async def after_slow_count():
+    print('done!')
 
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+    await slow_count.start()
 
 bot.run(token)
