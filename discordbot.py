96feedb053b2a2c1a5144a135017402650b04a73
botamoti64 <85117940+botamoti64@users.noreply.github.com>
2021-06-07 22:39:38 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import os
 import traceback
+import discord
 
 bot = commands.Bot(command_prefix='s>')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -19,7 +20,7 @@ async def ping(ctx):
 @bot.command()
 async def syamu(ctx):
     await ctx.send('ウイイイイイイイッッッッスどうも、シャムでーす。まぁ今日はオフ会、当日ですけども。えーとですね、まぁ集合場所の、えーイオンシネマに行ってきたんですけども、ただいまの時刻は1時を回りました。はい、ちょっと遅れて来たんですけどもね。えー11時ちょっとすぎくらいに、えーイオンシネマに行ったんですけども。ほんでーまぁイオンシネマの全体の動画を撮った後に行ったんですけども。スィー。ほんでーかれこれまぁ二時間くらい、えー待ったんですけども参加者は誰一人来ませんでした。ガチャ。誰一人来ることなかったですぅ。残念ながら。はい。一人くらい来るやろうなーと思ってたんですけども、スゥー、結局二時間くらい待っても誰一人来ませんでしたね、えぇ。')
-import discord
+
 client = discord.Client()
 @client.event
 async def on_message(message):
