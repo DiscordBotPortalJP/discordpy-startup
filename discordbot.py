5948a588805e4f3ae9290b384b8f25e982b0ac64
botamoti64 <85117940+botamoti64@users.noreply.github.com>
2021-06-20 21:02:18 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -30,7 +30,7 @@ async def on_message_delete(message):
 client = discord.Client()
 @client.event
 async def on_message(message):
- if message.content == hello:
-    await message.send('hello')
+ if message.content == (hello):
+    message.channel.send('hello')
     
 bot.run(token)
