5bb7d064ecd0c8dc062a95199495efafbe11ff62
Unico417 <82880551+Unico417@users.noreply.github.com>
2021-06-25 21:33:15 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,6 +7,15 @@ token = os.environ['DISCORD_BOT_TOKEN']
 
 
 @bot.event
+
+async def on_message(message):
+    if message.content.startswith("おはよう"):
+        if client.user != message.author:
+            m = "おはようございます" + message.author.name + "さん！"
+    await client.send_message(message.channel, m)
+
+
+@bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
