3f333a9eec007f4a670c0f3264db3bc842469f1e
sumaneko <77718010+sumaneko@users.noreply.github.com>
2021-07-07 21:55:16 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -163,7 +163,7 @@ async def on_message(message):
 				if datetime.datetime.now() < t + datetime.timedelta(seconds=ct):
 					return
 				#global ct_flag
-				ct_ flag = true
+				ct_flag = true
 				#セリフの文字列取得
 				msg = get_bot_reaction(message, ct_flag)
 
