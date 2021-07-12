a84cce7bbad616aa4bff3603223d235a26b426ee
sumaneko <77718010+sumaneko@users.noreply.github.com>
2021-07-13 01:13:33 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -94,6 +94,12 @@ async def sex(ctx):
 	#瀕死じゃないなら喋る
 	if bot_chan.dying_hp < bot_chan.get_hp():
 		await ctx.send(f"せっくす！")
+@bot.command()
+async def test(ctx):
+	await ctx.send(ctx.message.author.nick)	
+	#瀕死じゃないなら喋る
+	if bot_chan.dying_hp < bot_chan.get_hp():
+		#await ctx.send(f"今はテストはないみたいです")
 
 @bot.command()
 async def harapan(ctx):
