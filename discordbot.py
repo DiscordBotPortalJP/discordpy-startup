638809b0bb91dbfcf8c3412ff8f9860000dacbc4
Rqixy <67447178+Rqixy@users.noreply.github.com>
2021-07-30 19:25:10 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,7 +11,15 @@ async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
-
+    
+@bot.event
+async def on_message(message):
+    #メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    #「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/gollira':
+        await message.channel.send('うほうほ')
 
 @bot.command()
 async def ping(ctx):
