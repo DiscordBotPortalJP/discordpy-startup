68fd9c468095359f7a2b662a21a3392ac908015b
Shun Tannai <1ntegrale9uation@gmail.com>
2021-08-09 14:27:34 +0900

    [Fix] トークンの取得方法をよりPythonicに

--- discordbot.py
+++ discordbot.py
@@ -1,9 +1,8 @@
 from discord.ext import commands
-import os
+from os import getenv
 import traceback
 
 bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
 
 
 @bot.event
@@ -17,5 +16,5 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
-
+token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
