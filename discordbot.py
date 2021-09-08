6ad6e872b11c3a0de160b55073e00e5e14b09d57
hakudes <90252956+hakudes@users.noreply.github.com>
2021-09-08 16:08:42 +0900

    :
    :

--- discordbot.py
+++ discordbot.py
@@ -18,4 +18,13 @@ async def ping(ctx):
 
 
 token = getenv('DISCORD_BOT_TOKEN')
+
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
 bot.run(token)
