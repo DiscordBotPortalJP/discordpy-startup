113961688e9149e30e79533ca77be351666db32c
hakudes <90252956+hakudes@users.noreply.github.com>
2021-09-08 15:43:31 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -18,4 +18,24 @@ async def ping(ctx):
 
 
 token = getenv('DISCORD_BOT_TOKEN')
+
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
+
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
+
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
+        
 bot.run(token)
